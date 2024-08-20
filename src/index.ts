27ec1b027cf70d8cb9c692a8b142e0094ad7093e
minsoftk <minsoftk@gmail.com>
2024-08-20 18:19:29 +0900

    Feat: SPA 라우터 설계
    - 페이지별 분리
    - 라우터, 렌더링 함수 생성
    - Mockup 데이터 연결
    Resolves #

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-import { detailTest } from './pages/detail/detail';
-import { mainTest } from './pages/main/main';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NotFound from './pages/NotFound';
+import Slash from './pages/Slash';
+import Simplicity from './pages/Simplicity';
 
-console.log('🚀 ~ main:', mainTest());
-console.log('🚀 ~ main:', detailTest());
+export const navigate = (url: string) => {
+	window.history.pushState(null, null, url);
+	render();
+};
+
+export const createElement = (domString: string) => {
+	const $temp = document.createElement('template');
+	$temp.innerHTML = domString;
+	return $temp.content;
+};
+
+export const render = async () => {
+	const $root = document.getElementById('root');
+	const routes = [
+		{
+			path: '/',
+			component: Home,
+		},
+		{
+			path: '/detail',
+			component: Detail,
+		},
+		{
+			path: '/slash',
+			component: Slash,
+		},
+		{
+			path: '/simplicity',
+			component: Simplicity,
+		},
+	];
+	const path = window.location.pathname;
+	console.log('🚀 ~ render ~ path:', path);
+	const component = routes.find((route) => route.path === path)?.component || NotFound;
+
+	$root.replaceChildren(await component());
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+	render();
+});
+
+window.addEventListener('popstate', () => {
+	render();
+});
---
+++ src/pages/Slash.ts
@@ -0,0 +1,6 @@
+import { navigate, createElement } from '../index';
+
+export default function Slash() {
+	return createElement(`<div style="font-size:25px; padding:10px;"> Slash</div>
+    `);
+}
